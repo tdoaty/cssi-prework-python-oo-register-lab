3418c5748134fa119b1e528f526cc5425ec57759
tdoaty <tdoaty@google.com>
2017-05-25 13:31:39 -0400

    updated

--- register.py
+++ register.py
@@ -4,17 +4,19 @@ class CashRegister:
         self.total = 0
         self.items = []
     def add_item(self, item, price, quantity = 1):
+        self.last_quantity = quantity
+        self.last_price_total = (price * quantity)
         for i in range(quantity):
             self.items.append(item)
         self.total += (price * quantity)
     def apply_discount(self):
         if self.discount > 0:
-            self.total = self.total - ((self.discount / 100) * self.total)
-            return "After the discount, the total comes to " + str(self.total) + "."
+            quotient = self.discount / 100.00
+            self.total = self.total - (quotient * self.total)
+            return "After the discount, the total comes to $%.2f."
         else:
            return "There is no discount to apply."
      def void_last_transaction(self):
-         self.recent_item_price= (price * quantity)
-         self.total = self.total - self.recent_item_total
-         for i in range(quantity):
+         self.total -= self.last_price_total
+         for i in range(self.last_quantity):
               self.items.pop()
